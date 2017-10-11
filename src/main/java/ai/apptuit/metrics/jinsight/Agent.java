f051a05d63d81b57972509b660f35365912ab5f5
Rajiv Shivane <rajiv.shivane@agilx.ai>
2017-10-11 15:30:45 +0530

    Removed boot:byteman.jar javaagent option to simplify deployment

--- src/main/java/ai/apptuit/metrics/jinsight/Agent.java
+++ src/main/java/ai/apptuit/metrics/jinsight/Agent.java
@@ -16,9 +16,14 @@
 
 package ai.apptuit.metrics.jinsight;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.instrument.Instrumentation;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.function.BiConsumer;
+import java.util.jar.JarFile;
 import org.jboss.byteman.agent.Main;
 
 /**
@@ -29,6 +34,7 @@ import org.jboss.byteman.agent.Main;
  */
 public class Agent {
 
+  private static final String BYTEMAN_JAR_RESOURCE_NAME = "META-INF/boot/byteman.jar";
   private static final String BTM_SCRIPTS_RESOURCE_PATH = "btm-scripts/jinsight.btm";
   private static final String MODULE_LOADER_CLASSNAME =
       "ai.apptuit.metrics.jinsight.ContextualModuleLoader";
@@ -78,7 +84,36 @@ public class Agent {
       agentArgs = AGENT_PARAMS;
     }
 
+    JarFile bytemanJar = createBytemanJar();
+    if (bytemanJar == null) {
+      System.err.println("Could not locate: " + BYTEMAN_JAR_RESOURCE_NAME);
+      System.err.println("Agent will not be started.");
+      return;
+    }
+
+    instrumentation.appendToBootstrapClassLoaderSearch(bytemanJar);
     delegate.accept(agentArgs, instrumentation);
+  }
+
+  private static JarFile createBytemanJar() {
+    ClassLoader classLoader = Agent.class.getClassLoader();
+    InputStream bytemanStream = classLoader.getResourceAsStream(BYTEMAN_JAR_RESOURCE_NAME);
+    if (bytemanStream == null) {
+      return null;
+    }
+    try {
+      File bytemanJar = File.createTempFile("byteman", ".jar");
+      bytemanJar.deleteOnExit();
+      long numBytes = Files.copy(bytemanStream, bytemanJar.toPath(),
+          StandardCopyOption.REPLACE_EXISTING);
+      //System.out.println("bytemanJar = " + bytemanJar);
+      //System.out.println("numBytes = " + numBytes);
+      return new JarFile(bytemanJar);
+    } catch (IOException e) {
+      //TODO Log
+      e.printStackTrace();
+    }
+    return null;
   }
 
   private static class AgentInitializationException extends RuntimeException {
