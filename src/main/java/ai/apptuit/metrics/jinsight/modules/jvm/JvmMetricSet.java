35d780372c4951dd47187238d91deddebd8bf1c4
Rajiv <rajiv.shivane@agilx.ai>
2018-05-15 13:36:26 +0530

    Moved Guage classes into corresponding MetricSet

--- src/main/java/ai/apptuit/metrics/jinsight/modules/jvm/JvmMetricSet.java
+++ src/main/java/ai/apptuit/metrics/jinsight/modules/jvm/JvmMetricSet.java
@@ -16,18 +16,26 @@
 
 package ai.apptuit.metrics.jinsight.modules.jvm;
 
+import static java.lang.management.ManagementFactory.OPERATING_SYSTEM_MXBEAN_NAME;
+import static java.lang.management.ManagementFactory.getPlatformMBeanServer;
+import static java.lang.management.ManagementFactory.getRuntimeMXBean;
+import static java.lang.management.ManagementFactory.newPlatformMXBeanProxy;
+
+import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.MetricSet;
 import com.codahale.metrics.jvm.CachedThreadStatesGaugeSet;
 import com.codahale.metrics.jvm.ClassLoadingGaugeSet;
 import java.io.IOException;
+import java.lang.management.RuntimeMXBean;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.management.MBeanServerConnection;
 
 /**
  * @author Rajiv Shivane
@@ -69,5 +77,35 @@ public class JvmMetricSet implements MetricSet {
 
   private String getMetricName(String prefix, String key) {
     return MetricRegistry.name(prefix, key).toLowerCase().replace('-', '_');
+  }
+
+  private static class UptimeGauge implements Gauge<Long> {
+
+    private final RuntimeMXBean rtMxBean = getRuntimeMXBean();
+
+    @Override
+    public Long getValue() {
+      return rtMxBean.getUptime();
+    }
+  }
+
+  private static class ProcessCpuTicksGauge implements Gauge<Long> {
+
+    private final com.sun.management.OperatingSystemMXBean osMxBean;
+
+    public ProcessCpuTicksGauge() throws ClassNotFoundException, IOException {
+
+      MBeanServerConnection mbsc = getPlatformMBeanServer();
+      Class.forName("com.sun.management.OperatingSystemMXBean");
+
+      osMxBean = newPlatformMXBeanProxy(mbsc, OPERATING_SYSTEM_MXBEAN_NAME,
+          com.sun.management.OperatingSystemMXBean.class);
+
+    }
+
+    @Override
+    public Long getValue() {
+      return osMxBean.getProcessCpuTime();
+    }
   }
 }
