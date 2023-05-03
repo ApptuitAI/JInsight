736144567ecb8bc210854ac3a4f983ca84d8fce7
Jaya Gupta <jaya@inceptioncloud.io>
2023-05-03 16:48:53 +0530

    removed unused imports

--- src/test/java/ai/apptuit/metrics/jinsight/ConfigServiceTest.java
+++ src/test/java/ai/apptuit/metrics/jinsight/ConfigServiceTest.java
@@ -23,8 +23,6 @@ import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.net.URL;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
